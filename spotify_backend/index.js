3c82021e43a2b234607095320b1f51cdb635d2c7
Nipun899 <nipunjain899@gmail.com>
2024-01-03 15:42:35 +0530

    Established conncetion between nodeJS and MongoDB cluster

--- spotify_backend/index.js
+++ spotify_backend/index.js
@@ -1,13 +1,30 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
-const port = 8008
+const port = 3000;
+//connecting our database to node package
+//mongoose.connect("<database ka url>",{<connections options and modifications>})
+mongoose.connect(
+  "mongodb+srv://nipunjain892:" +
+    process.env.mongoPassword +
+    "@cluster0.soacbs9.mongodb.net/?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+).then((x)=>{
+    console.log("Connected to Mongo")
 
-app.get("/", (req,res) => {
-    //req contain all the data for request 
-    // res contain all the data for response
-    res.send ("Hello World")
+}).catch((err)=>{
+console.log("error while connecting")
 });
-// now we want to tell the server to run on which port 
-app.listen(port,()=>{
-    console.log("This server is running on port " + port)
-})
+app.get("/", (req, res) => {
+  //req contain all the data for request
+  // res contain all the data for response
+  res.send("Hello World");
+});
+// now we want to tell the server to run on which port
+app.listen(port, () => {
+  console.log("This server is running on port " + port);
+});
